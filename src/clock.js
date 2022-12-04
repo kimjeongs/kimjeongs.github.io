2194b1da48400f1a06b016ff567e928f851b4687
jeongseung <kimjeongs03@gmail.com>
2022-12-04 17:16:30 +0900

    시계수정

--- src/clock.js
+++ src/clock.js
@@ -4,13 +4,14 @@ const clock = document.querySelector(".clock");
 function time() {
 	const date = new Date();
 	let hour = date.getHours();
-	const minutes = date.getMinutes();
-	const seconds = date.getSeconds();	
+	const minutes = String(date.getMinutes()).padStart(2,"0");
+	const seconds = String(date.getSeconds()).padStart(2,"0");	
 	let ampm;
+	console.log(typeof minutes)
 
 	if ( hour > 12) {
 		ampm = "PM";
-		hour = hour - 12;
+		hour = String(hour - 12).padStart(2,"0");
 	} else {
 		ampm = "AM";
 	}
