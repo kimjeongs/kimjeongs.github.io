951ea74b593e9885ebe2a0e2c2dc313d2772c116
kjs <kimjeongs03@gmail.com>
2024-03-14 14:22:09 +0900

    수정

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # kimjeongs.github.io
+
+# UX 퍼블 내부 프로젝트
--- src/background.js
+++
@@ -1,6 +0,0 @@
-const imgArr = ["1.jpeg","2.jpeg","3.jpeg"];
-const img = document.createElement("img");
-const random =  Math.floor(Math.random()*imgArr.length);
-
-img.src = `img/${imgArr[random]}`;
-document.body.appendChild(img);
