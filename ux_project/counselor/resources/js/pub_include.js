d07f1519d5d4d879342055b097b0c4956077d1ee
kjs <kimjeongs03@gmail.com>
2024-03-25 17:45:55 +0900

    카운셀러 커밋

---
+++ ux_project/counselor/resources/js/pub_include.js
@@ -0,0 +1,35 @@
+var baseURL = "/ux_project/counselor/resources";
+var include = {
+  meta: function () {
+    document.write('<meta charset="UTF-8">');
+    document.write('<meta http-equiv="X-UA-Compatible" content="IE=edge">');
+    document.write(
+      '<meta name="viewport" content="width=device-width, initial-scale=1.0">'
+    );
+  },
+  title: function () {
+    document.write('<title>counselor</title>');
+  },
+  styles: function () {
+    document.write(
+      '<link rel="stylesheet" href="' + baseURL + '/css/style.css" type="text/css" />'
+    );
+  },
+  scripts: function () {
+    // library js
+    document.write(
+      '<script src="' + baseURL + '/js/jquery-3.6.0.min.js"></script>'
+    );
+    document.write(
+      '<script src="' + baseURL + '/js/gsap.min.js"></script>'
+    );
+    document.write(
+      '<script src="' + baseURL + '/js/ScrollTrigger.min.js"></script>'
+    );
+    
+    // custom js
+    document.write(
+      '<script src="' + baseURL + '/js/common.js"></script>'
+    );
+  },
+};
